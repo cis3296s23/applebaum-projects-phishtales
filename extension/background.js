1faf3a52e7555ede080ef3582860e685681a89f0
trau3 <70736675+trau3@users.noreply.github.com>
2023-04-19 18:25:34 -0400

    Update extension blocked feature

--- extension/background.js
+++ extension/background.js
@@ -1,4 +1,5 @@
 var whitelistMap = new Map();
+var whitelistTabs = {};
 
 // const { getDomain, updateIgnoreList, loadWhitelist, getIgnore } = require('./domainUtil')
 import {getDomain, updateIgnoreList, loadWhitelist, getIgnore} from './domainUtil.js'
@@ -26,23 +27,26 @@ chrome.runtime.onStartup.addListener(
 chrome.webNavigation.onCommitted.addListener(async (callback) => {
   if (callback.transitionType && callback.transitionType != 'auto_toplevel' && callback.transitionType != 'auto_subframe' ) {
     console.log(`${callback.url} ${callback.frameId} ${callback.tabId} ${callback.parentFrameId} ${callback.transitionType}`);
+
+    if (callback.url.startsWith(chrome.runtime.getURL("blocked.html"))) {
+      return; // ignore blocked page
+    }
   
     var phishing;
     //send call to model to see if phishing
-    await fetch("https://www.phishtales.net/extension", {
+    var response = await fetch("http://127.0.0.1:5000/extension", {//https://www.phishtales.net
       method: "POST",
       body: JSON.stringify({
-        form: {
-          url: callback.url,
-          returnString: true
-        }
+          url: callback.url
       }),
       headers: {
         "Content-type": "application/json; charset=UTF-8"
       }
-    })
-  .then((response) => response.json())
-  .then((json) => phishing = json);
+    });
+
+    phishing = await response.text();
+
+  
 
 
     console.log(phishing);
@@ -50,13 +54,15 @@ chrome.webNavigation.onCommitted.addListener(async (callback) => {
     var isWhitelisted = ignoreListContains(callback.url);
     console.log(`whitelsited : ${isWhitelisted}`);
 
-    if (phishing == 'phishing' && !isWhitelisted) {
+    if (phishing == 'phishing' && !isWhitelisted && whitelistTabs[callback.tabId] == undefined) {
         chrome.action.setIcon({path: "/warning.png"});
         chrome.tabs.update(callback.tabId, {url: (chrome.runtime.getURL("blocked.html") + "?url=" + callback.url)});
         
-    } else if (!isWhitelisted) {
+    } else {
         chrome.action.setIcon({path: "/PhishTales.png"});
     }
+
+    whitelistTabs[callback.tabId] = undefined;
 
   }
 });
@@ -71,6 +77,7 @@ chrome.runtime.onMessage.addListener(
         "from a content script:" + sender.tab.url :
         "from the extension");
       whitelistMap.set(request.url, 0)
+      whitelistTabs[request.tabId] = true
     } else if (request.action == 'updateIgnore') {
       updateIgnoreList(request.url, request.add);
     } else if (request.action == 'getIgnore') {
--- extension/blocked.js
+++ extension/blocked.js
@@ -1,10 +1,18 @@
 const urlString = window.location.search;
 const urlParams = new URLSearchParams(urlString);
-console.log(urlParams.get('url'));
+var currentTabId;
 
+
+//display website name to user
 infoLabel = document.getElementById('info');
+infoLabel.innerHTML = `PhishTales has blocked ${getPhishingURL()}<br> This website is most likely phishing.`;
 
-infoLabel.innerHTML = `PhishTales has blocked ${urlParams.get('url')}<br> This website is most likely phishing.`;
+//get current tab id for whitelisting when clicking continue - website may redirect and prevent you from loading the next webpage
+chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+    console.log(tabs[0]);
+    currentTabId = tabs[0].id
+});
+
 
 
 function goBack() {
@@ -12,8 +20,9 @@ function goBack() {
 }
 
 function continueToURL() {
-    chrome.runtime.sendMessage({action: "whitelist", url: urlParams.get('url')});
-    chrome.tabs.update({url: urlParams.get('url')});
+    
+    chrome.runtime.sendMessage({action: "whitelist", url: getPhishingURL(), tabId: currentTabId});
+    chrome.tabs.update({url: getPhishingURL()});
 }
 
 function getPhishingURL() {
